ced3e6e708e065a29e6b8db6d1b75f94bd3f5d9a
tawhkd272 <Yawbk283@protonmail.com>
2021-06-06 21:40:16 +0530

    commit messadjs

---
+++ kiki.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+RED="\e[31m"
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
+
+u="c"
+while [ $u = "c" ]
+do
+   now=$(date +"%T")
+   echo "Current time : $now"
+   echo -e "${YELLOW}current time :$now ${ENDCOLOR}"
+   touch teemy.txt
+   sleep 5
+   git add .
+   sleep 5
+   git commit -m "commit messadjs" 
+   sleep 5
+   
+   now=$(date +"%T")
+   echo "Current time : $now"
+   echo -e "${YELLOW}current time :$now ${ENDCOLOR}"
+   sleep 600
+done
+
+
+
